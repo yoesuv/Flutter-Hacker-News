5c7fe59e4ed3861ae55fbc9b1c265fccdd01e2ae
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-11-13 16:00:21 +0700

    add db provider

--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -4,3 +4,54 @@ import 'dart:io';
 import 'package:path/path.dart';
 import 'dart:async';
 import '../models/item_model.dart';
+
+class NewsDbProvider {
+    Database db;
+
+    init() async {
+        Directory documentsDirectory = await getApplicationDocumentsDirectory();
+        final path = join(documentsDirectory.path, 'items.db');
+        db = await openDatabase(
+            path,
+            version: 1,
+            onCreate: (Database newDb, int version) {
+                newDb.execute("""
+                    CREATE TABLE Items(
+                        id INTEGER PRIMARY KEY,
+                        type TEXT,
+                        by TEXT,
+                        time INTEGER,
+                        text TEXT,
+                        parent INTEGER,
+                        kids BLOB,
+                        dead INTEGER,
+                        deleted INTEGER,
+                        url TEXT,
+                        score INTEGER,
+                        title TEXT,
+                        descendants INTEGER
+                    )
+                """);
+            }
+        );
+    }
+
+    fetchItem(int id) async {
+        final maps = await db.query(
+            "Items",
+            columns: null,
+            where: "id =?",
+            whereArgs: [id]
+        );
+
+        if (maps.length > 0) {
+            return ItemModel.fromDb(maps.first);
+        }
+        return null;
+    }
+
+    addItem(ItemModel item) {
+        return db.insert("Items", item.toMap());
+    }
+
+}
