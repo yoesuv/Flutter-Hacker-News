316def931883fcfbeec8c8c9e5372393eb65c2c2
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-11-22 10:38:57 +0700

    app boilerplate

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 void main() {
-    
+    runApp(App());
 }
---
+++ lib/src/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'screens/news_list.dart';
+
+class App extends StatelessWidget {
+
+    Widget build(context) {
+        return MaterialApp (
+            title: 'News',
+            home: NewsList()
+        );
+    }
+
+}
