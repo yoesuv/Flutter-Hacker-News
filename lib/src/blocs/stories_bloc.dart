9442b0d7a604af6bfc4acf4ac0b307b3db25dced
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-11-22 16:37:06 +0700

    create provider

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,3 +1,16 @@
 import 'package:rxdart/rxdart.dart';
+import '../models/item_model.dart';
+import '../resources/repository.dart';
 
-class StoriesBloc{}
+class StoriesBloc{
+
+    final _topIds = PublishSubject<List<int>>();
+
+    Observable<List<int>> get topIds => _topIds.stream;
+
+
+    dispose() {
+        _topIds.close();
+    }
+
+}
