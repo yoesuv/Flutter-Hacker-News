d44ac3adb154f8cd78bf64f1f74bcbc1acc8034e
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-11-28 15:18:17 +0700

    add circular progressbar

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -4,10 +4,15 @@ import '../resources/repository.dart';
 
 class StoriesBloc{
 
+    final _repository = Repository();
     final _topIds = PublishSubject<List<int>>();
 
     Observable<List<int>> get topIds => _topIds.stream;
 
+    fetchTopIds() async {
+        final ids = await _repository.fetchTopIds();
+        _topIds.sink.add(ids);
+    }
 
     dispose() {
         _topIds.close();
--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import '../blocs/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
     Widget build(context) {
+        final bloc = StoriesProvider.of(context);
+        bloc.fetchTopIds();
         return Scaffold(
             appBar: AppBar(
                 title: Text('Top News')
             ),
-            body: Text('Show Top News here!')
+            body: buildList(bloc)
+        );
+    }
+
+    Widget buildList(StoriesBloc bloc) {
+        return StreamBuilder(
+            stream: bloc.topIds,
+            builder: (context, AsyncSnapshot<List<int>> snapshot) {
+                if (!snapshot.hasData) {
+                    return Center(
+                        child: CircularProgressIndicator()
+                    );
+                }
+                return ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, int index){
+                        return Text('${snapshot.data[index]}');
+                    }
+                );
+            }
         );
     }
 }
