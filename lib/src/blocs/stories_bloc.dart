ce7f7d6a65d2cc61852b67bb10726aeb614c4c7b
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-11-28 16:41:52 +0700

    add item transformer

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:rxdart/rxdart.dart';
 import '../models/item_model.dart';
 import '../resources/repository.dart';
@@ -12,6 +13,16 @@ class StoriesBloc{
     fetchTopIds() async {
         final ids = await _repository.fetchTopIds();
         _topIds.sink.add(ids);
+    }
+
+    _itemTransformer() {
+        return ScanStreamTransformer(
+            (Map<int, Future<ItemModel>> cache, int id, index) {
+                cache[id] = _repository.fetchItem(id);
+                return cache;
+            },
+            <int, Future<ItemModel>>{}
+        );
     }
 
     dispose() {
