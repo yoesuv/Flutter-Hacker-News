dcb2f8baa10bf7113b72f4dadba7f9e810a5c9c3
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-12-04 13:52:58 +0700

    detail news screen

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/news_list.dart';
+import 'screens/news_detail.dart';
 import 'blocs/stories_provider.dart';
 
 class App extends StatelessWidget {
@@ -8,15 +9,26 @@ class App extends StatelessWidget {
         return StoriesProvider(
             child: MaterialApp (
                 title: 'News',
-                onGenerateRoute: (RouteSettings settings) {
-                    return MaterialPageRoute(
-                        builder: (context) {
-                            return NewsList();
-                        }
-                    );
-                }
+                onGenerateRoute: routes
             )
         );
+    }
+
+    Route routes(RouteSettings settings) {
+        if (settings.name == '/') {
+            return MaterialPageRoute(
+                builder: (context) {
+                    return NewsList();
+                }
+            );
+        } else {
+            return MaterialPageRoute(
+                builder: (context) {
+                    return NewsDetail();
+                }
+            );
+        }
+
     }
 
 }
---
+++ lib/src/screens/news_detail.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NewsDetail extends StatelessWidget {
+
+    Widget build(context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text('Detail News')
+            ),
+            body: Text('Detail Screen')
+        );
+    }
+
+}
