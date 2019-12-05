fea9e6924acbbc86ab91c0408636031b64996576
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-12-05 10:05:05 +0700

    show title news detail screen

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -32,10 +32,25 @@ class NewsDetail extends StatelessWidget {
                         if (!itemSnapshot.hasData) {
                             return Text('Loading');
                         }
-                        return Text(itemSnapshot.data.title);
+                        return buildTitle(itemSnapshot.data);
                     }
                 );
             }
+        );
+    }
+
+    Widget buildTitle(ItemModel item) {
+        return Container(
+            margin: EdgeInsets.all(10.0),
+            alignment: Alignment.topCenter,
+            child: Text(
+                item.title,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold
+                )
+            )
         );
     }
 
