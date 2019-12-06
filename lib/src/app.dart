97704fb0afb3e18598d0e8938b267e46083ef749
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-12-06 09:46:35 +0700

    refactor request stories bloc

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -21,6 +21,8 @@ class App extends StatelessWidget {
         if (settings.name == '/') {
             return MaterialPageRoute(
                 builder: (context) {
+                    final storiesBloc = StoriesProvider.of(context);
+                    storiesBloc.fetchTopIds();
                     return NewsList();
                 }
             );
--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -6,7 +6,6 @@ import '../widgets/refresh.dart';
 class NewsList extends StatelessWidget {
     Widget build(context) {
         final bloc = StoriesProvider.of(context);
-        bloc.fetchTopIds();
         return Scaffold(
             appBar: AppBar(
                 title: Text('Top News')
